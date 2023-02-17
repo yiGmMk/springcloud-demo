ee6de6a7fb49fbafcd2addc7811b81a43d29317e
yi_t <marvelousme@163.com>
2023-02-17 18:55:27 +0800

    初始化sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,15 @@
-CREATE TABLE if not exists customer  (
-    id SERIAL PRIMARY KEY, 
-    first_name VARCHAR(255), 
-    last_name VARCHAR(255));
+CREATE TABLE if not exists customer (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255)
+);
+CREATE TABLE if not exists person (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    age int
+);
+CREATE TABLE if not exists department (
+    id SERIAL PRIMARY KEY,
+    dept VARCHAR(255),
+    emp_id int
+);
