59bdcf327810b3dc4cc255797998970334399dbf
yiGmMk <marvelousme@163.com>
2023-02-17 16:20:17 +0800

    log config

--- src/main/java/com/xwb/learn/controllers/ShopController.java
+++ src/main/java/com/xwb/learn/controllers/ShopController.java
@@ -1,7 +1,9 @@
 package com.xwb.learn.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,13 +13,23 @@ import com.xwb.learn.domain.Department;
 import com.xwb.learn.domain.DepartmentCurdRepository;
 import com.xwb.learn.domain.DepartmentRepository;
 
+import jakarta.validation.constraints.Min;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Configuration
 @RestController
 @CrossOrigin(origins = "*")
+@ConfigurationProperties(prefix = "learn.pagination")
+@Validated
 public class ShopController {
+
+    @Min(value = 10, message = "分页数量最小为5")
+    private int pagesize = 10;
+
+    public void setPagesize(int pagesize) {
+        this.pagesize = pagesize;
+    }
 
     @Autowired
     DepartmentRepository shoprepo;
